e9b0e44ad537572e1b1145b728ba36dbf80cb06e
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-12 18:30:30 +0900

    파일 생성

---
+++ 2020/소스.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("----------------------");
+	printf("\n11월 코로나 확진자 분석");
+	printf("\n----------------------");
+	printf("\n원하는 버튼을 누르세요");
+	printf("\n1. 주차별 분석");
+	printf("\n2. 지역별 분석");
+	printf("\n3. 확진자 명단");
+	printf("\n4. 끝내기");
+
+
+
+
+
+
+	return 0;
+}
